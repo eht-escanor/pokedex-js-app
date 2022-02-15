59e5791c94474583b261ffd3fca46e2e04a6f9a4
eht-escanor <99154713+eht-escanor@users.noreply.github.com>
2022-02-15 13:24:34 -0500

    added pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+pokemonList = [
+  {
+    name: "Bulbasaur",
+    type: "Seed",
+    height: 0.7,
+  };
+  {
+    name: "Squirtle",
+    type: "Water",
+    height: 0.5,
+  };
+  {
+    name: "Charmander",
+    type: "Fire",
+    height: 0.6,
+  };
+]
