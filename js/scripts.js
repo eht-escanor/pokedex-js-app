b72ba93faaafb36a346cac8d50a46d54ee031080
eht-escanor <99154713+eht-escanor@users.noreply.github.com>
2022-02-16 09:43:48 -0500

    changed type field from string to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 pokemonList = [
   {
     name: "Bulbasaur",
-    type: "Seed",
+    type: ["Seed"],
     height: 0.7,
   };
   {
     name: "Squirtle",
-    type: "Water",
+    type: ["Water"],
     height: 0.5,
   };
   {
     name: "Charmander",
-    type: "Fire",
+    type: ["Fire"],
     height: 0.6,
   };
 ]
