21a5a31e984ae3c7f0e4591ef0ec9df91cf46cc7
eht-escanor <99154713+eht-escanor@users.noreply.github.com>
2022-02-16 23:26:46 -0500

    changed semicolons in object array to commas

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ pokemonList = [
     name: "Bulbasaur",
     type: ["Seed"],
     height: 0.7,
-  };
+  },
   {
     name: "Squirtle",
     type: ["Water"],
     height: 0.5,
-  };
+  },
   {
     name: "Charmander",
     type: ["Fire"],
     height: 0.6,
-  };
+  },
 ]
