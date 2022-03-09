a8ade8a317555330a5d22395affc1ca216a63d4c
eht-escanor <99154713+eht-escanor@users.noreply.github.com>
2022-03-08 22:45:23 -0500

    changed pokemonList to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
-pokemonList = [
-  {
-    name: "Bulbasaur",
-    type: ["Seed"],
-    height: 0.7,
-  },
-  {
-    name: "Squirtle",
-    type: ["Water"],
-    height: 0.5,
-  },
-  {
-    name: "Charmander",
-    type: ["Fire"],
-    height: 0.6,
-  },
-  {
-    name: "Charizard",
-    type:["Fire"],
-    height: 1.7,
-  }
-]
+let pokemonRepository = (function(){
+  pokemonList = [
+    {   
+      name: "Bulbasaur",
+      type: ["Seed"],
+      height: 0.7,
+    },
+    {
+      name: "Squirtle",
+      type: ["Water"],
+      height: 0.5,
+    },
+    {
+      name: "Charmander",
+      type: ["Fire"],
+      height: 0.6,
+    },
+    {
+      name: "Charizard",
+      type:["Fire"],
+      height: 1.7,
+    }
+  ]
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
 //iterate through list of pokemon and display name and height
 // for (let i = 0; i < pokemonList.length; i++){
 // //conditional to test for height to print "- Wow that's big!"
@@ -31,7 +41,7 @@ pokemonList = [
 // };
 
 //forEach equivalent of above code
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
   if (pokemon.height > 1.0) {
     document.write(pokemon.name + " (height: " + pokemon.height + ") " + "- Wow that's big! <br/>");
   }else {
